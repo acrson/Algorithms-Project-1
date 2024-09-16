2ca0a111c59dc8daf1a28794f73b49b860786a49
SY52804 <syang5@cub.uca.edu>
2024-09-16 18:09:32 -0500

    ippie
    yay

---
+++ algorithmsProject1.py
@@ -0,0 +1,49 @@
+"""
+
+FUNCTIONS UP HERE,,,
+
+MAIN ON BOTTOM
+
+"""
+
+
+
+
+
+
+
+# define main function// front end
+def main():
+    print("\nRSA keys have been generated.\nPlease select your user type:")
+    print("      1. A public user")
+    print("      2. Owner of key")
+    print("      3. Exit program")
+    choice = input("Enter your choice: ")
+    
+    # figure out this loop Sera
+   # add while lopp or smth 
+    if (choice == 1):
+        print("As a public user, what would you like to do?")
+        print("      1. A public user")
+        print("      2. Owner of key")
+        print("      3. Exit program")
+        choice = input("Enter your choice: ")
+    elif (choice == 2):
+        print ("hi")
+    elif (choice == 3):
+        print("hi")
+    else:
+        
+    
+    
+    
+    
+    
+    
+
+
+
+
+# call main function
+main()
+
