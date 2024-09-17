280d423c9ed0dbdde03c3492a311e1f0ab1c40c5
SY52804 <syang5@cub.uca.edu>
2024-09-16 19:40:40 -0500

    tryna push new stuff
    I was editing on the wrong version of this file and i spent a whole hour digging thru my files to find the path to spyder when I could have just got INTO spyder and opened the file there to make the commit thing show up on github. Sigh, what a time.

--- algorithmsProject1.py
+++ algorithmsProject1.py
@@ -20,30 +20,27 @@ def main():
     print("      3. Exit program")
     choice = input("Enter your choice: ")
     
-    # figure out this loop Sera
-   # add while lopp or smth 
-    if (choice == 1):
-        print("As a public user, what would you like to do?")
-        print("      1. A public user")
-        print("      2. Owner of key")
-        print("      3. Exit program")
-        choice = input("Enter your choice: ")
-    elif (choice == 2):
-        print ("hi")
-    elif (choice == 3):
-        print("hi")
-    else:
+    # verification thingy for user type
+    # uh, read ur notes Sera, in "Scratch'
+    # we are doing this on paper first :[ 
+    bool = False
+    while(bool == False):
+        if (choice == 1):
+            print("As a public user, what would you like to do?")
+            print("      1. Send an encrypted message")
+            print("      2. Authenticate a digital signature")
+            print("      3. Exit")
+            # add snippet thingy
+            bool = True
+        elif (choice == 2):
+            print ("hi")
+        elif (choice == 3):
+            print("hi")
+        else:
+            print("hi")
         
     
     
-    
-    
-    
-    
-
-
-
-
 # call main function
 main()
 
