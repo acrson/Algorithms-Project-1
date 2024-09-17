c63f8d60d0c757f1f9e7fa3ec491de02e1adf487
SY52804 <syang5@cub.uca.edu>
2024-09-17 16:35:07 -0500

    figuring out main shenanigans pt 1
    yes

--- algorithmsProject1.py
+++ algorithmsProject1.py
@@ -14,33 +14,52 @@ MAIN ON BOTTOM
 
 # define main function// front end
 def main():
-    print("\nRSA keys have been generated.\nPlease select your user type:")
-    print("      1. A public user")
-    print("      2. Owner of key")
-    print("      3. Exit program")
-    choice = input("Enter your choice: ")
-    
-    # verification thingy for user type
-    # uh, read ur notes Sera, in "Scratch'
-    # we are doing this on paper first :[ 
-    bool = False
-    while(bool == False):
-        if (choice == 1):
-            print("As a public user, what would you like to do?")
-            print("      1. Send an encrypted message")
-            print("      2. Authenticate a digital signature")
-            print("      3. Exit")
-            # add snippet thingy
-            bool = True
-        elif (choice == 2):
-            print ("hi")
-        elif (choice == 3):
-            print("hi")
-        else:
-            print("hi")
-        
+   
+   bool = False
+   while(bool == False):
+       print("\nRSA keys have been generated.\nPlease select your user type:")
+       print("      1. A public user")
+       print("      2. Owner of key")
+       print("      3. Exit program")
+       choice = input("Enter your choice: ")
+       
+       if (choice == 1):
+           # Public User
+           print("As a public user, what would you like to do?")
+           print("      1. Send an encrypted message")
+           print("      2. Authenticate a digital signature")
+           print("      3. Exit")
+       elif (choice == 2):
+           # Owner of key
+           print("As the Owner of the Keys, what would you like to do?")
+           print("      1. Decrypt a received message")
+           print("      2. Digitally sign a message")
+           print("      3. Show key")
+           print("      4. Generate new set of key")
+           print("      5. Exit")
+       elif (choice == 3):
+           # Exit program
+           print("Bye for now!")
+           bool = True 
+            
+           
     
     
 # call main function
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
